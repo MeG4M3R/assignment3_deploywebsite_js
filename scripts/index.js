cd4dc31533c260a34ca08e1ff88ba55abd7ce3bd
Henry Nguyen <henryho335@gmail.com>
2025-12-12 20:52:17 -0700

    header/footer code

---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+hamburgerButton = document.querySelector(".hamburger-button");
+hamburgerList = document.querySelector(".hamburger-list");
+hamburgerButton.addEventListener("click", () => {
+  hamburgerList.classList.toggle("show");
+});
+// Credits to https://www.youtube.com/watch?v=gOXYcXKVl1c for help with the hamburger menu code.
